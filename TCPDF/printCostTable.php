57e74b2111f95f2be6e63759a5ba38851db6032d
Janelle Cueto <janellecueto@outlook.com>
2019-04-22 16:18:59 -0500

    commented printCostTable.php

--- TCPDF/printCostTable.php
+++ TCPDF/printCostTable.php
@@ -56,15 +56,15 @@ switch($table){
 function addHeader($y){
     global $pdf, $isObs;
     $pdf->SetFont('helvetica', 'b',11);
-    $pdf->SetTextColor(255,255,255);
+    $pdf->SetTextColor(255,255,255);                                //set text color to white (it is on top of blue)
 
-    $pdf->Rect(7,$y,263,7, 'F', array(), array(30, 144, 255));
-    $pdf->SetLineWidth(0.4);
+    $pdf->Rect(7,$y,263,7, 'F', array(), array(30, 144, 255));      //draw the blue header box
+    $pdf->SetLineWidth(0.4);                                        //draw top, left, right border lines
     $pdf->Line(7,$y,270,$y);
     $pdf->Line(7, $y, 7, $y+7);
     $pdf->Line(270, $y, 270, $y+7);
 
-    $pdf->SetLineWidth(0.25);
+    $pdf->SetLineWidth(0.25);                                       //bottom border line   
     $pdf->Line(7, $y+7, 270, $y+7);
 
     if($isObs){
@@ -82,64 +82,54 @@ function addHeader($y){
         $pdf->Line(153, $y, 153, $y+7);
         $pdf->Text(155, $y+1, "ESTIMATED CAPITAL EXPENDITURES");
     }
-    $pdf->SetTextColor(0,0,0);
+    $pdf->SetTextColor(0,0,0);                                      //reset text color
 }
 
 //this function adds the name/issue and description/action to the table. calls addCostSummary function to add Estimated Capital Expenditures
 function addItemRow($row){
     global $pdf, $conn, $globalY, $isObs;
-    if($globalY > 190){
-        $pdf->AddPage("L", "LETTER");
-        addHeader(8);
-        $pdf->SetFont('helvetica', '', 9);
+    if($globalY > 190){                                             //add new page if we are getting close to the bottom
+        $pdf->AddPage("L", "LETTER");                       
+        addHeader(8);                                               //add header at top of new page
+        $pdf->SetFont('helvetica', '', 9);                          //reset our y point
         $globalY = 15;
     }
 
     $y = $globalY;
     $hasCost = false;
+    $costRow = []; 
 
 //    query into costs table to get total and costs row
-    if($row['idcosts']) {
+    if($row['idcosts']) {                                           //grab cost information if exists
         $costQ = "SELECT * FROM hospital.costs WHERE idcosts = " . $row['idcosts'];
         $costR = $conn->query($costQ);
         $costRow = $costR->fetch_array();
         $hasCost = true;
-    }
-    else $costRow = [];
+    }                                    
 
-
-    if($isObs){
+    if($isObs){                                                     //again, observations table has slightly different layout
         $pdf->MultiCell(72, 14, $row['issue_violation'], 0, '', 0, 1, 8, $y+1, 1, 0, 0, 1, 4, 'm', 1 );
         $pdf->MultiCell(72, 14, $row['recommended_action'], 0, '', 0, 1, 81, $y+1, 1, 0, 0, 1, 4, 'm', 1 );
-        $pdf->SetFont('helvetica', '', 9);
-        if($hasCost) addCostSummary($costRow);      //insert cost summary breakdown
-        else{
-            //just add lines if cost does not exist
-            $pdf->SetLineWidth(0.4);
-            $pdf->Line(7,$y,7,$y+15);
-            $pdf->Line(270, $y, 270, $y+15);
-            $pdf->SetLineWidth(0.25);
-            $pdf->Line(48,$y,48, $y+15);
-            $pdf->Line(153,$y,153,$y+15);
-            $pdf->Line(7,$y+15,270,$y+15);
-            $globalY += 15;
-        }
     }
     else{
         $pdf->MultiCell(42, 14, $row['name'], 0, '', 0, 1, 8, $y+1, 1, 0, 0, 1, 4, 'm', 1 );
         $pdf->MultiCell(100, 24, $row['comments'], 0, '', 0, 1, 50, $y+1, 1, 0, 0, 1, 4, 'm', 1 );
-        $pdf->SetFont('helvetica', '', 9);
-        if($hasCost) addCostSummary($costRow);
-        else{
-            $pdf->SetLineWidth(0.4);
-            $pdf->Line(7,$y,7,$y+15);
-            $pdf->Line(270, $y, 270, $y+15);
-            $pdf->SetLineWidth(0.25);
-            $pdf->Line(48,$y,48, $y+15);
-            $pdf->Line(153,$y,153,$y+15);
-            $pdf->Line(7,$y+15,270,$y+15);
-            $globalY += 15;
-        }
+    }
+    $pdf->SetFont('helvetica', '', 9);
+    if($hasCost) addCostSummary($costRow);                      //insert cost summary breakdown
+    else{
+        //just add lines if cost does not exist
+        $pdf->SetLineWidth(0.4);
+        $pdf->Line(7,$y,7,$y+15);
+        $pdf->Line(270, $y, 270, $y+15);
+        $pdf->SetLineWidth(0.25);
+
+        if($isObs) $pdf->Line(81,$y,81, $y+15);
+        else $pdf->Line(48,$y,48,$y+15);
+
+        $pdf->Line(153,$y,153,$y+15);
+        $pdf->Line(7,$y+15,270,$y+15);
+        $globalY += 15;
     }
 
 }
@@ -155,7 +145,7 @@ function addCostSummary($costRow){      //instead of passing isObs, pass a start
     else $xCol = 48;
 
     $pdf->SetFont('helvetica', 'b', 9);
-    $pdf->Text(155, $y+1, "ACTIVITY");
+    $pdf->Text(155, $y+1, "ACTIVITY");              //add sub-table headers
     $pdf->Text(246, $y+1, "GUESTIMATE");
     $pdf->SetFont('helvetica', '', 9);
 
@@ -170,11 +160,11 @@ function addCostSummary($costRow){      //instead of passing isObs, pass a start
     $activityR = $conn->query($activityQ);
 
     $y += 6;
-    while($aRow = $activityR->fetch_array()){
+    while($aRow = $activityR->fetch_array()){       //add each activity line item
         if($y>200){
-            $pdf->Line(7, $y, 270, $y);
-            $pdf->AddPage("L", "LETTER");
-            addHeader(8);
+            $pdf->Line(7, $y, 270, $y);             //if we get towards the bottom, add a new page and header
+            $pdf->AddPage("L", "LETTER");           //and reset local $y variable (we'll reset global once we're
+            addHeader(8);                           //done adding the cost summary)
             $pdf->SetFont('helvetica', '', 9);
             $y = 15;
         }
@@ -184,7 +174,7 @@ function addCostSummary($costRow){      //instead of passing isObs, pass a start
         $pdf->MultiCell(23, 6, "$".number_format($aRow['cost'], "0", ".", ","), 0, 'R', 0, 1, 244, $y, 1, 0, 0, 1, 4, 'm', 1 );
 
         $pdf->SetLineWidth(0.4);
-        $pdf->Line(7, $y, 7,$y+6);
+        $pdf->Line(7, $y, 7,$y+6);                  //draw table lines 
         $pdf->Line(270, $y, 270, $y+6);
         $pdf->SetLineWidth(0.25);
         $pdf->Line($xCol, $y, $xCol, $y+6);
@@ -193,8 +183,8 @@ function addCostSummary($costRow){      //instead of passing isObs, pass a start
         $y+=6;
     }
 
-    if($y > 190){
-        $pdf->Line(7, $y, 270, $y);
+    if($y > 190){                                   //after adding the activities, we have to add 3 more rows.
+        $pdf->Line(7, $y, 270, $y);                 //add a page if we're close to the bottom
         $pdf->AddPage("L", "LETTER");
         addHeader(8);
         $y = 15;
@@ -228,8 +218,8 @@ function addCostSummary($costRow){      //instead of passing isObs, pass a start
 
 
     $pdf->SetFont('helvetica', '', 9);
-    $globalY =$y;
-    $pdf->Line(7, $globalY, 270, $globalY);
+    $globalY =$y;                                   //reset our global y variable
+    $pdf->Line(7, $globalY, 270, $globalY);         //draw bottom line for the page row 
 }
 
 
@@ -281,13 +271,13 @@ $globalY = 22;
 $pdf->SetFont('helvetica', '', 9);
 $pdf->SetTextColor(0,0,0);
 
-$runningTotal = 0;
+$runningTotal = 0;      //this keeps track of the running total of all of the costs
 
 if($isObs){
     $query = "SELECT * FROM hospital.observations WHERE hospital = '$hospital'";
 }
 else {
-    if ($hospital === 'NSE') {
+    if ($hospital === 'NSE') {          //this hospital specifically needs more conditions
         $query = "SELECT * FROM hospital.$table WHERE hospital = '$hospital' AND `condition` > 4 OR `condition` = 0";
     } else {
         $query = "SELECT * FROM hospital.$table WHERE hospital = '$hospital' AND `condition` = 6 OR `condition` = 0";
@@ -295,12 +285,12 @@ else {
 }
 $result = $conn->query($query);
 while($row = $result->fetch_array()){
-    addItemRow($row);
+    addItemRow($row);          
 }
 
 //add bottom total row:
-$y = $globalY;
-if($y>204){
+$y = $globalY;                  //when we're done adding all of our rows,
+if($y>204){                     //add another page if there's not enough room for the last 'total' row
     $pdf->AddPage("L", "LETTER");
     $y=7;
 }
